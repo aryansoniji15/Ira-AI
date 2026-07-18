92ecc8a44c5b906036c632b66fbd808b1c924aee
aryansoniji15 <aryansoniji15@gmail.com>
2026-07-19 00:28:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+},{
+threshold:0.15
+});
+
+
+sections.forEach(section=>{
+
+section.classList.add("hidden");
+
+observer.observe(section);
+
+});
+const glow = document.querySelector(".cursor-glow");
+
+
+document.addEventListener("mousemove",(e)=>{
+
+glow.style.left = e.clientX + "px";
+
+glow.style.top = e.clientY + "px";
+
+});
+const imageUpload = document.getElementById("imageUpload");
+
+imageUpload.addEventListener("change", function(){
+
+const file = this.files[0];
+
+if(file){
+
+  const reader = new FileReader();
+
+reader.onload = function(e){
+
+document.getElementById("previewImage").src = e.target.result;
+
+document.getElementById("imageStatus").innerHTML =
+"Ira AI is analyzing your image... 🧠";
+
+
+setTimeout(()=>{
+
+document.getElementById("imageStatus").innerHTML =
+"Analysis complete. Visual intelligence activated ✓";
+
+},3000);
+
+}
+
+reader.readAsDataURL(file);
+
+}
+
+});
+const text = "Hello! I am Ira AI. I can help you create, analyze images, and solve problems.";
+
+let index = 0;
+
+function typeAI(){
+
+if(index < text.length){
+
+document.getElementById("aiTyping").innerHTML += text.charAt(index);
+
+index++;
+
+setTimeout(typeAI,50);
+
+}
+
+}
+
+typeAI();
+const sendBtn = document.getElementById("sendBtn");
+
+sendBtn.onclick = function(){
+
+const message = document.getElementById("userMessage").value;
+
+
+if(message){
+
+document.getElementById("chatMessages").innerHTML +=
+
+`
+<div class="user-message-box">
+${message}
+</div>
+
+<div class="ai-message-box">
+Ira AI: I understand your question. This is a demo AI response.
+</div>
+`;
+
+document.getElementById("userMessage").value="";
+
+}
+
+};
+
+import { auth } from "./firebase.js";
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const messageBox = document.getElementById("authMessage");
+
+document.getElementById("signupBtn").addEventListener("click", () => {
+  createUserWithEmailAndPassword(
+    auth,
+    emailInput.value,
+    passwordInput.value
+  )
+  .then(() => {
+    messageBox.innerText = "Account created successfully!";
+  })
+  .catch((error) => {
+    messageBox.innerText = error.message;
+  });
+});
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+  signInWithEmailAndPassword(
+    auth,
+    emailInput.value,
+    passwordInput.value
+  )
+  .then(() => {
+    messageBox.innerText = "Login successful!";
+  })
+  .catch((error) => {
+    messageBox.innerText = error.message;
+  });
+});
